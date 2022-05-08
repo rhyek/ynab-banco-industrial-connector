368e2758b558ae422c5893c6d77a7b62b933988f
Carlos González <carlos.rgn@gmail.com>
2022-05-08 12:57:42 -0600

    include swiftshader

--- src/YnabBancoIndustrialConnectorBackend/Infrastructure/BancoIndustrialScraper/src/BancoIndustrialScraperService.cs
+++ src/YnabBancoIndustrialConnectorBackend/Infrastructure/BancoIndustrialScraper/src/BancoIndustrialScraperService.cs
@@ -66,7 +66,7 @@ public class BancoIndustrialScraperService
         ExecutablePath = "/lambda-chromium/chromium",
         Args = new[] {
           // https://github.com/GoogleChrome/chrome-launcher/blob/master/docs/chrome-flags-for-tools.md
-          
+
           // these are already present by default
           // "--disable-background-networking",
           // "--disable-background-timer-throttling",
@@ -89,7 +89,7 @@ public class BancoIndustrialScraperService
           // "--no-sandbox",
           // "--password-store=basic",
           // "--use-mock-keychain",
-          
+
           // these are new from: https://github.com/JupiterOne/playwright-aws-lambda/blob/main/src/chromium.ts#L24
           // "--autoplay-policy=user-gesture-required", // no
           // "--disable-component-update", // no
@@ -104,11 +104,34 @@ public class BancoIndustrialScraperService
           // "--ignore-gpu-blacklist", // no
           // "--no-default-browser-check", // no
           // "--no-pings", // no
-          // "--no-zygote", // no
-          "--use-gl=swiftshader", // no
-          // "--single-process", // no
-          // mine
-          "--in-process-gpu",
+          // // "--no-zygote", // no
+          // "--use-gl=swiftshader", // no
+          // // "--single-process", // no
+          // // mine
+          // "--in-process-gpu",
+
+          "--allow-running-insecure-content", // https://source.chromium.org/search?q=lang:cpp+symbol:kAllowRunningInsecureContent&ss=chromium
+          "--autoplay-policy=user-gesture-required", // https://source.chromium.org/search?q=lang:cpp+symbol:kAutoplayPolicy&ss=chromium
+          "--disable-component-update", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableComponentUpdate&ss=chromium
+          "--disable-domain-reliability", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableDomainReliability&ss=chromium
+          "--disable-features=AudioServiceOutOfProcess,IsolateOrigins,site-per-process", // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+          "--disable-print-preview", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisablePrintPreview&ss=chromium
+          "--disable-setuid-sandbox", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSetuidSandbox&ss=chromium
+          "--disable-site-isolation-trials", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSiteIsolation&ss=chromium
+          "--disable-speech-api", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableSpeechAPI&ss=chromium
+          "--disable-web-security", // https://source.chromium.org/search?q=lang:cpp+symbol:kDisableWebSecurity&ss=chromium
+          "--disk-cache-size=33554432", // https://source.chromium.org/search?q=lang:cpp+symbol:kDiskCacheSize&ss=chromium
+          "--enable-features=SharedArrayBuffer", // https://source.chromium.org/search?q=file:content_features.cc&ss=chromium
+          "--hide-scrollbars", // https://source.chromium.org/search?q=lang:cpp+symbol:kHideScrollbars&ss=chromium
+          "--ignore-gpu-blocklist", // https://source.chromium.org/search?q=lang:cpp+symbol:kIgnoreGpuBlocklist&ss=chromium
+          "--in-process-gpu", // https://source.chromium.org/search?q=lang:cpp+symbol:kInProcessGPU&ss=chromium
+          "--mute-audio", // https://source.chromium.org/search?q=lang:cpp+symbol:kMuteAudio&ss=chromium
+          "--no-default-browser-check", // https://source.chromium.org/search?q=lang:cpp+symbol:kNoDefaultBrowserCheck&ss=chromium
+          "--no-pings", // https://source.chromium.org/search?q=lang:cpp+symbol:kNoPings&ss=chromium
+          "--no-sandbox", // https://source.chromium.org/search?q=lang:cpp+symbol:kNoSandbox&ss=chromium
+          "--no-zygote", // https://source.chromium.org/search?q=lang:cpp+symbol:kNoZygote&ss=chromium
+          "--use-gl=swiftshader", // https://source.chromium.org/search?q=lang:cpp+symbol:kUseGl&ss=chromium
+          "--window-size=1920,1080", // https://source.chromium.org/search?q=lang:cpp+symbol:kWindowSize&ss=chromium
         }
       });
 
