c9464401c4d6fab85fdd23c7750bf807c3051857
Carlos González <carlos.rgn@gmail.com>
2022-05-15 11:29:58 -0600

    missing .js extension

--- infra/deploy/src/index.js
+++ infra/deploy/src/index.js
@@ -1,5 +1,5 @@
 //@ts-check
 export { scrapeBankTransactionsSqsUrl } from './sqs-scrape-bank-transactions.js';
-export { playwrightTracesBucketName } from './playwright-traces-s3-bucket';
+export { playwrightTracesBucketName } from './playwright-traces-s3-bucket.js';
 export * from './lambdas/http-api/index.js';
 export * from './lambdas/scrape-bank-transactions-consumer/index.js';
