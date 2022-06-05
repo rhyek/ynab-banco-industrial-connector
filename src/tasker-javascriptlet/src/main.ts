997f4e7a778e7126edbbc144a40fc31eb9987e1e
Carlos González <carlos.rgn@gmail.com>
2022-06-05 12:51:40 -0600

    update lambda origin

--- src/tasker-javascriptlet/src/main.ts
+++ src/tasker-javascriptlet/src/main.ts
@@ -5,11 +5,12 @@ async function main() {
   while (true) {
     try {
       // 10 second timeout:
-  		// const controller = new AbortController();
-  		// const timeoutId = setTimeout(() => controller.abort(), 5_000);
-      
-      const domain = 'https://ynab-controller.rhyek.ngrok.io';
-      const url = `${domain}/mobile-app-notifications/register-new`;
+      // const controller = new AbortController();
+      // const timeoutId = setTimeout(() => controller.abort(), 5_000);
+
+      const origin = 'https://txm44kpo64.execute-api.us-east-1.amazonaws.com';
+      const path = 'stage/new-push-notification-tx';
+      const url = `${origin}/${path}`;
       const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -27,17 +28,17 @@ async function main() {
         const text = await response.text();
         const message = {
           status: response.status,
-          text
-        }
-      	throw new Error(JSON.stringify(message));
+          text,
+        };
+        throw new Error(JSON.stringify(message));
       }
       exit(); // exit because of success
     } catch (e: any) {
       error = e?.message;
       flash(error);
       // delay 1s
-      await new Promise(resolve => setTimeout(resolve, 1_000));
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
     }
   }
-}  
+}
 main();
