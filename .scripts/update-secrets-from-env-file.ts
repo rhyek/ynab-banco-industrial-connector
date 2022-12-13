0eb53033341f2ae6353cc86c3c2de89e5fbf6a2e
Carlos González <carlos.rgn@gmail.com>
2022-12-12 19:59:13 -0600

    changed card

--- .scripts/update-secrets-from-env-file.ts
+++ .scripts/update-secrets-from-env-file.ts
@@ -1,15 +1,17 @@
 #!/usr/bin/env ts-node-transpile-only
 import { config } from 'dotenv';
-import { execaCommand as execa } from 'execa';
+import { command } from 'execa';
 import { backendEnvironmentVariableKeys } from './consts/backend-environment-variable-keys';
 
 config();
 
-for (const key of backendEnvironmentVariableKeys) {
-  await execa(
-    `pulumi --cwd infra/deploy config set --secret ${key} ${process.env[key]}`,
-    {
-      stdio: 'inherit',
-    }
-  );
-}
+(async () => {
+  for (const key of backendEnvironmentVariableKeys) {
+    await command(
+      `pulumi --cwd infra/deploy config set --secret ${key} ${process.env[key]}`,
+      {
+        stdio: 'inherit',
+      }
+    );
+  }
+})();
